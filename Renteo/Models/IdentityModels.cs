193f5f821a0f5a39742848916cdc7c06b405d875
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-08-22 22:25:57 +0200

    Initialized Model in database

--- Renteo/Models/IdentityModels.cs
+++ Renteo/Models/IdentityModels.cs
@@ -9,6 +9,8 @@ namespace Renteo.Models
     // Możesz dodać dane profilu dla użytkownika, dodając więcej właściwości do klasy ApplicationUser. Odwiedź stronę https://go.microsoft.com/fwlink/?LinkID=317594, aby dowiedzieć się więcej.
     public class ApplicationUser : IdentityUser
     {
+        public DbSet<Customer> Customers { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Element authenticationType musi pasować do elementu zdefiniowanego w elemencie CookieAuthenticationOptions.AuthenticationType

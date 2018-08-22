292e60dcd3f70517a22e23418bfe7476641ad039
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-08-22 21:15:19 +0200

    Simple Vehicle class to test MVC pattern

---
+++ Renteo/Controllers/VehiclesController.cs
@@ -0,0 +1,24 @@
+﻿using Renteo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Renteo.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        // GET: Vehicles
+        public ActionResult Random()
+        {
+            var vehicle = new Vehicle()
+            {
+                Make = "Opel",
+                Model = "Insignia"
+            };
+
+            return View(vehicle);
+        }
+    }
+}

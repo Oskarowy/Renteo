97c4694a24e1fbb2cc0e3fd19f39b7aa521ba62c
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-08-22 21:41:18 +0200

    New Bootstrap theme

--- Renteo/App_Start/BundleConfig.cs
+++ Renteo/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Renteo
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-litera.css",
                       "~/Content/site.css"));
         }
     }

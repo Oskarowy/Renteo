0f17fb5c47a6008ac66f32b28292cac81bdff451
Oskar Białek <oskar.bialek.programista@gmail.com>
2018-08-24 23:01:48 +0200

    Implemented AutoMapper for API

---
+++ Renteo/App_Start/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Renteo.Dtos;
+using Renteo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Renteo.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customer, CustomerDto>();
+            CreateMap<CustomerDto, Customer>();
+
+        }
+    }
+}
